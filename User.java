eecfc7304a2f0f0f20404855607eb26484cd32be
Djoga24 <djoga1@ocdsb.ca>
2023-06-14 23:38:02 -0400

    Added contents of text file to the table

--- User.java
+++ User.java
@@ -1,11 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.nio.file.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class User {
     private JFrame userListFrame;
     private JEditorPane usersEditorPane;
 
-    
+    private static final String FILE_PATH = "accounts.txt";
+
     public void createUsersScreen() {
         userListFrame = new JFrame("User List");
         userListFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -28,11 +33,44 @@ public class User {
         JScrollPane scrollPane = new JScrollPane(usersEditorPane);
         userListPanel.add(scrollPane, BorderLayout.CENTER);
 
+        populateUserList();
         userListFrame.add(userListPanel);
         userListFrame.setVisible(true);
     }
 
     
+    
+private void populateUserList() {
+    try {
+        List<String> lines = Files.readAllLines(Paths.get(FILE_PATH));
+        List<String> filteredLines = lines.stream()
+                .map(String::trim)
+                .filter(line -> {
+                    String[] parts = line.split(",");
+                    return parts.length >= 3 && parts[2].trim().equalsIgnoreCase("User");
+                })
+                .collect(Collectors.toList());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><table>");
+        sb.append("<tr><th style='background-color:#ADD8E6; padding:5px;'>Username</th><th style='background-color:#ADD8E6; padding:5px;'>Password</th></tr>");
+
+        for (String line : filteredLines) {
+            String[] parts = line.split(",");
+            String storedUsername = parts[0];
+            String storedPassword = parts[1];
+
+            sb.append("<tr><td style='padding:5px;'>").append(storedUsername).append("</td><td style='padding:5px;'>")
+                    .append(storedPassword).append("</td><td style='padding:5px;'>");        }
+        sb.append("</table></html>");
+
+        if (usersEditorPane != null) {
+            usersEditorPane.setText(sb.toString());
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
